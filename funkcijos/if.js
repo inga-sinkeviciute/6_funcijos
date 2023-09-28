ac7348e762028497cc2b30780e8271bb65c762f3
Inga Sinkeviciute <isinkeviciute7@gmail.com>
2023-09-28 16:17:08 +0300

    additional example

--- funkcijos/if.js
+++ funkcijos/if.js
@@ -231,3 +231,14 @@ if (one > two) {
 } else {
 	console.log(`${two} yra didziausias skaicius`);
 }
+
+function isLeapYear(year) {
+	if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+const year = 2023;
+
+console.log(`${year} is a leap year: ${isLeapYear(year)}`);
